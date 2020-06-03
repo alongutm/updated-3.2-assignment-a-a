10d1a408ec9f2c772cd9e4afb0e395b7c0b40967
amitshakarchy <58327011+amitshakarchy@users.noreply.github.com>
2020-06-03 17:01:22 +0300

    added table to db

--- Backend/modules/routes/recipes.js
+++ Backend/modules/routes/recipes.js
@@ -1,38 +1,43 @@
 const express = require("express");
 const router = express.Router();
-const search_utils= require("../utils/search_recipes");
+const search_utils = require("../utils/search_recipes");
 const axios = require("axios");
 const api_domain = "https://api.spoonacular.com/recipes";
+const DBUtils = require("../utils/DBUtils");
+
 
 
 router.get("/recipeInfo", async (req, res, next) => {
     try {
-      const recipe = await getRecipeInfo(req.query.recipe_id);
-      let fullInfo= {
-        recipe_id: recipe.data.id,
-        recipeName: recipe.data.title,
-        image: recipe.data.image,
-        coockingTime: recipe.data.readyInMinutes,
-        numberOfLikes: recipe.data.aggregateLikes,
-        isVegan: recipe.data.vegan,
-        isVegeterian: recipe.data.vegetarian ,
-        isGlutenFree: recipe.data.glutenFree,
-        IngredientList: recipe.data.extendedIngredients.map(function(obj){
-            return obj.name;
-        }),
-        Instructions: recipe.data.summary,
-        MealsQuantity: recipe.data.servings,
-        seen: false,
-        isFavorite: false 
-      }
-      
-      res.send({ data: recipe.data });
-    } catch (error) {
-      next(error);
-    }
-  });
+        // get seen & isFavorite
+        const favorite = await DBUtils.execQuery(`SELECT * FROM favoriteRecipes WHERE user_id='${req.session.id}' and recipe_id='${req.query.recipe_id}'`);
+        let isFavorite= (favorite.length>0);
 
-  function getRecipeInfo(id) {
+        const recipe = await getRecipeInfo(req.query.recipe_id);
+        let fullInfo = {
+            recipe_id: recipe.data.id,
+            recipeName: recipe.data.title,
+            image: recipe.data.image,
+            coockingTime: recipe.data.readyInMinutes,
+            numberOfLikes: recipe.data.aggregateLikes,
+            isVegan: recipe.data.vegan,
+            isVegeterian: recipe.data.vegetarian,
+            isGlutenFree: recipe.data.glutenFree,
+            IngredientList: recipe.data.extendedIngredients.map(function (obj) {
+                return obj.name;
+            }),
+            Instructions: recipe.data.summary,
+            MealsQuantity: recipe.data.servings,
+            isFavorite: isFavorite
+        }
+
+        res.send({ data: fullInfo });
+    } catch (error) {
+        next(error);
+    }
+});
+
+function getRecipeInfo(id) {
     return axios.get(`${api_domain}/${id}/information`, {
       params: {
         includeNutrition: false,
@@ -40,7 +45,6 @@ router.get("/recipeInfo", async (req, res, next) => {
       }
     });
   }
-
 
 
 
---
+++ CREATE TABLE [dbo].[seen](.sql
@@ -0,0 +1,8 @@
+
+-- CREATE TABLE favoriteRecipes(
+--     [user_id] [varchar](300) NOT NULL,
+--     [recipe_id] int not null,
+--     PRIMARY KEY (user_id, recipe_id)
+-- )
+
+select * from favoriteRecipes;
