867226ea39065f6fb729941ecd1c49ed2f78e768
Alon <51872854+alongutm@users.noreply.github.com>
2020-06-03 17:05:05 +0300

    search fucntions

--- Backend/modules/routes/recipes.js
+++ Backend/modules/routes/recipes.js
@@ -26,10 +26,46 @@ router.get("/recipeInfo", async (req, res, next) => {
         isFavorite: false 
       }
       
-      res.send({ data: recipe.data });
+      res.send({ fullInfo });
     } catch (error) {
       next(error);
     }
+  });
+
+  router.get("/search", async (req, res, next) => {
+    try {
+      var recipesObj = await searchRecipeInfo(req);
+      var recipesArray = recipesObj.data.results;
+      var recipes = [];
+      // the "for-of" loop is aysnc
+      for(let recipe of recipesArray){
+        var fullRecipe = await getRecipeInfo(recipe.id);
+        let fullInfo= {
+          recipe_id: fullRecipe.data.id,
+          recipeName: fullRecipe.data.title,
+          image: fullRecipe.data.image,
+          coockingTime: fullRecipe.data.readyInMinutes,
+          numberOfLikes: fullRecipe.data.aggregateLikes,
+          isVegan: fullRecipe.data.vegan,
+          isVegeterian: fullRecipe.data.vegetarian ,
+          isGlutenFree: fullRecipe.data.glutenFree,
+          IngredientList: fullRecipe.data.extendedIngredients.map(function(obj){
+              return obj.name;
+          }),
+          Instructions: fullRecipe.data.summary,
+          MealsQuantity: fullRecipe.data.servings,
+          seen: false,
+          isFavorite: false 
+        }
+        recipes.push(fullInfo);
+      }
+
+
+    } catch (error) {
+      next(error);
+    }
+    res.send({ recipes });
+
   });
 
   function getRecipeInfo(id) {
@@ -41,6 +77,26 @@ router.get("/recipeInfo", async (req, res, next) => {
     });
   }
 
+  function searchRecipeInfo(req) {
+    var query =  req.query;
+    var paramNum = query.number;
+    if(paramNum == null){
+      req.query.number = 5;
+      paramNum = 5;
+    } 
+    
+    
+    return axios.get(`${api_domain}/search`, {
+      params: {
+        apiKey: process.env.spooncular_apiKey,
+        query: query.query,
+        number: paramNum,
+        ...(query.diet ? {diet: query.diet} : {}),
+        ...(query.cuisine ? {cuisine: query.cuisine} : {}),
+        ...(query.intolerances ? {intolerances: query.intolerances} : {})
+      }
+    });
+  }
 
 
 
