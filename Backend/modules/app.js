f149b6336cdd441a7275638f442a5257cda30819
Alon <51872854+alongutm@users.noreply.github.com>
2020-07-12 12:55:08 +0300

    little fix

--- Backend/modules/app.js
+++ Backend/modules/app.js
@@ -25,7 +25,7 @@ app.use(
   session({
     cookieName: "session", // the cookie key name
     secret: process.env.COOKIE_SECRET, // the encryption key
-    duration: 60 60 60 * 1000, // expired after 60 minutes
+    duration: 60* 60 *60 * 1000, // expired after 60 minutes
     activeDuration: 0 // if expiresIn < activeDuration,
     //the session will be extended by activeDuration milliseconds
   })
